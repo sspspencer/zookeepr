6c520c215807189fcc73988245221016b2023f3c
Spencer Pichette <ssptuningperformance@gmail.com>
2022-08-02 19:01:09 -0400

    create web server

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ function filterByQuery(query, animalsArray) {
   }
   return filteredResults;
 }
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
 
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -52,6 +56,14 @@ app.get("/api/animals", (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(PORT, () => {
